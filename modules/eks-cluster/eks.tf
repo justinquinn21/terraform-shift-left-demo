2c34578ea0cb8123e22adc2cc593f17f5522c798
Alan Nix <alanneilnix@gmail.com>
2024-02-05 10:47:23 -0500

    chore: pinning EKS module to prevent provider version conflicts

--- modules/eks-cluster/eks.tf
+++ modules/eks-cluster/eks.tf
@@ -1,5 +1,6 @@
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
 
   cluster_name                   = local.cluster_name
   cluster_version                = local.cluster_version
