d8195079fd00d996927e3b7bc2bb3752b34c25d5
Alan Nix <alan.nix@wiz.io>
2024-06-03 15:14:13 -0400

    fix: removing Wiz terraform provider dependency (#13)

--- modules/eks-services/versions.tf
+++ modules/eks-services/versions.tf
@@ -2,15 +2,11 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
+      version = ">= 2.0"
     }
     helm = {
       source  = "hashicorp/helm"
       version = ">= 2.7"
-    }
-    wiz = {
-      version = "~> 1.1"
-      source  = "tf.app.wiz.io/wizsec/wiz"
     }
   }
 }
