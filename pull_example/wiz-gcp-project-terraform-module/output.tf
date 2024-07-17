be3fb97f2c3994f56eed89d735cf832a0157be0a
Justin Quinn <justinquinn51691@gmail.com>
2024-07-17 08:38:28 -0400

    push

--- pull_example/wiz-gcp-project-terraform-module/output.tf
+++
@@ -1,7 +0,0 @@
-output "cloud_events_topic" {
-  value = var.cloud_events ? module.wiz_gcp_cloud_events[0].cloud_events_topic : ""
-}
-
-output "cloud_events_subscription_id" {
-  value = var.cloud_events ? module.wiz_gcp_cloud_events[0].cloud_events_subscription_id : ""
-}
